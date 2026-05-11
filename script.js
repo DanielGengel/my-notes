14e8ac3d57f5f617a305955a620796b567a9beb9
DanielGengel <daniel.gengel@outlook.com>
2026-05-11 17:52:46 +0200

    Template for notes added, farben als klasse und aus html entfernt

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 const myNotesContainer = document.getElementById("myNotes");
+const noteInputCard = document.getElementById("noteInputCard");
 const noteTitleInput = document.getElementById("noteTitleInput");
 const noteTextInput = document.getElementById("noteTextInput");
+const btnSaveNote = document.getElementById("buttonSaveNoteID");
+const noteColorSelection = document.getElementById("noteColorSelectionID");
 
 // Create empty array or parse if array already available
 let myNotes = JSON.parse(localStorage.getItem("myNotes")) || [];
-let selectedNoteColor = "#fff59d";
 
+// Default color
+let selectedNoteColor = "yellow";
+let previousNoteColor = "yellow";
+
+// Background colors for notes
+const colors = ["yellow", "green", "blue", "pink", "gray"];
+
+// saveNote without brackets to not run the function at file load
+btnSaveNote.addEventListener("click", saveNote);
+
+function init() {
+    addColorButtons();
+    renderNotes();
+    noteInputCard.classList.add(selectedNoteColor);
+}
 
 function renderNotes() {
+    let html = "";
     myNotesContainer.innerHTML = "";
 
     for (let i = 0; i < myNotes.length; i++) {
-        const noteHTML = `
-            <article class="noteCard" style="background-color: ${myNotes[i].noteColor}">
-                <h3>${myNotes[i].noteTitle}</h3>
-                <p>${myNotes[i].noteText.substring(0, 120)}</p>
-            </article>
-        `;
-
-        myNotesContainer.innerHTML += noteHTML;
-        console.log(noteHTML);
-        
+        console.log(getNoteTemplate(i));
+        html += getNoteTemplate(i);
     }
-
+    myNotesContainer.innerHTML = html;
 }
 
-
-
-
+function getNoteTemplate(index) {
+    return `
+            <article class="noteCard ${myNotes[index].noteColor}">
+                <h3>${myNotes[index].noteTitle}</h3>
+                <p>${myNotes[index].noteText.substring(0, 120)}</p>
+            </article>
+        `;
+}
 
 function saveNote() {
     const noteTitle = noteTitleInput.value.trim();
@@ -56,11 +71,24 @@ function clearNoteInputs() {
     noteTextInput.value = "";
 }
 
-
-
 function selectNoteColor(color) {
     selectedNoteColor = color;
-    noteInputCard.style.backgroundColor = color;
+    noteInputCard.classList.remove(previousNoteColor);
+    noteInputCard.classList.add(color);
+    previousNoteColor = color;
+    console.log(selectedNoteColor);
 }
 
+function addColorButtons() {
+    for (let i = 0; i < colors.length; i++) {
+        const button = document.createElement("button");
+        button.classList.add("colorButton", colors[i]);
 
+        button.addEventListener("click", () => {
+            selectNoteColor(colors[i]);
+            
+            
+        });
+        noteColorSelection.appendChild(button);
+    }
+}
